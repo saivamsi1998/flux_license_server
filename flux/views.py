81fd0106bce260533a479f9479b89ed3a1fd183f
Bhavan Turaka <bturaka@adobe.com>
2023-09-11 10:18:01 +0530

    server initial setup

---
+++ flux/views.py
@@ -0,0 +1,6 @@
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+
+@csrf_exempt 
+def index(request):
+    return HttpResponse("Hello, world. You're at the flux index.")
