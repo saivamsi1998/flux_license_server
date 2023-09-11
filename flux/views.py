f8a4ec0b72eae941bc553295681b4857ffdcaafa
Bhavan Turaka <bturaka@adobe.com>
2023-09-12 00:09:37 +0530

    Modified reg, dereg, heartbeat funcs - working

--- flux/views.py
+++ flux/views.py
@@ -2,187 +2,209 @@ from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 
 from django.utils import timezone
+from .models import User, Offer, OfferAppMap, OfferUserMap, License, LicenseAppMap
+
+import json
 
 HEARTBEAT_TIME = 60 #min
 BUFFER_TIME = 5 #min
 
+def json_response(err_msg="", require_profile_status = True):
+	json_obj = {}
+	if err_msg=="" or err_msg=="success":
+		if require_profile_status:
+			json_obj['profile_status'] = "PROFILE_AVAIALBLE"
+		json_obj['status'] = "success"
+		err_msg = ""
+	else:
+		if require_profile_status:
+			json_obj['profile_status'] = "PROFILE_DENIED"
+		json_obj['status'] = "fail"
+		json_obj['err_msg'] = err_msg
+	return HttpResponse(json.dumps(json_obj))
+
 @csrf_exempt 
 def index(request):
-	return HttpResponse("Hello, world. You're at the flux index.")
+	return json_response("Hello, world. You're at the flux index.")
 
-
+@csrf_exempt
 def reg(request):
-	license_id = request.POST["license_id"]
-	user_id = request.POST["user_id"]
-	app_id = request.POST["app_id"]
-	device_id = request.POST["device_id"]
+	user_id = request.POST["userId"]
+	app_id = request.POST["nglAppId"]
+	device_id = request.POST["deviceId"]
 
 	# verify if the user, app is part of the license
+	qs1 = OfferUserMap.objects.filter(user__user_id = user_id).values("offer_id")
+	qs2 = OfferAppMap.objects.filter(app_id = app_id).values("offer_id")
+	qs = qs1.intersection(qs2)
+	if(qs.count()==0):
+		return json_response("Offer isn't available for given (user_id, app_id)= (%s,%s)"%(user_id, app_id))
+	offer = Offer.objects.get(pk = qs[0]['offer_id'])
+	print("Using offer: %s for given (user_id, app_id)= (%s,%s)"%(str(offer),user_id, app_id))
+	offerusermap_entry = offer.offerusermap_set.get(user__user_id = user_id)
+	user = offerusermap_entry.user
+	license_id = offer.license_id
+
+	# license update
+	query_set = License.objects.filter(offer = offer, user = user, device_id = device_id)
+	query_set_count = query_set.count()
+
+	if (query_set_count >= 2):
+		return json_response("Multiple license for same (user, device) tuple found. Something went wrong")
+	if (query_set_count  == 0):
+		# check and allocate license and add license entry
+		# mutex might be required when updating active_license_count
+		if (offer.active_license_count >= offer.max_license_count):
+			return json_response("Pool license limit reached")
+		else:
+			offer.active_license_count += 1
+			offer.save()
+			license = License.objects.create(offer = offer, user = user, device_id = device_id)
+			print("Created Pool License entry for (license_id, user_id, device_id) = (%s,%s,%s)"%(license_id, user_id, device_id))
+	elif (query_set_count  == 1):
+		license = query_set[0]
+	# add/update license app map entry
 	try:
-		offer = Offer.objects.get(license_id = license_id)
-		offerusermap_entry = offer.offerusermap_set.get(user__user_id = user_id)
-		user = offerusermap_entry.user
-		offer.offerappmap_set.get(app_id = app_id)
+		licenseappmap_entry = license.licenseappmap_set.get(app_id = app_id)
+		licenseappmap_entry.last_active_time = timezone.now()
+		licenseappmap_entry.save()
+	except (KeyError, LicenseAppMap.DoesNotExist):
+		license.licenseappmap_set.create(app_id = app_id, last_active_time = timezone.now())
+	print("Created/Updated LicenseAppMap entry due to expired heartbeat for (license_id, user_id, device_id, app_id) =  (%s,%s,%s,%s)"%(license_id, user_id, device_id, app_id))
+	# TODO: Schedule next heartbeat check and remove entry
+	return json_response("success")
 
-		# license update
-		query_set_count = License.objects.filter(offer = offer, user = user, device_id = device_id).count()
-		if (query_set_count  == 0):
-			# check and allocate license and add license entry
-			# mutex might be required when updating active_license_count
-			if (offer.active_license_count >= offer.max_license_count):
-				return HttpResponse("Pool license limit reached")
-			else:
-				offer.active_license_count += 1
-				offer.save()
-				license = License.objects.create(offer = offer, user = user, device_id = device_id)
-				print("Created Pool License entry for (license_id, user_id, device_id) =  (%s,%s,%s,%s)"%(license_id, user_id, device_id))
-		# add/update license app map entry
-		if (query_set_count == 0 or query_set_count == 1):
-			try:
-				licenseappmap_entry = license.licenseappmap_set.get(app_id = app_id)
-				licenseappmap_entry.last_active_time = timezone.now()
-				licenseappmap_entry.save()
-			except (KeyError, LicenseAppMap.DoesNotExist):
-				license.licenseappmap_set.create(app_id = app_id, last_active_time = timezone.now())
-			print("Created/Updated LicenseAppMap entry due to expired heartbeat for (license_id, user_id, device_id, app_id) =  (%s,%s,%s,%s)"%(license_id, user_id, device_id, app_id))
-			# TODO: Schedule next heartbeat check and remove entry
-		if (query_set_count >= 2):
-			return HttpResponse("Multiple license for same (user, device) tuple found. Something went wrong")
-
-	except (KeyError, Offer.DoesNotExist):
-		return HttpResponse("Offer isn't available for given license id")
-	except (KeyError, OfferUserMap.DoesNotExist):
-		return HttpResponse("Offer isn't available for given user id")
-	except (KeyError, OfferAppMap.DoesNotExist):
-		return HttpResponse("Offer isn't available for given app id")
-
+@csrf_exempt
 def heartbeat(request):
-	license_id = request.POST["license_id"]
-	user_id = request.POST["user_id"]
-	app_id = request.POST["app_id"]
-	device_id = request.POST["device_id"]
+	user_id = request.POST["userId"]
+	app_id = request.POST["nglAppId"]
+	device_id = request.POST["deviceId"]
 
 	# verify if the user, app is part of the license
+	qs1 = OfferUserMap.objects.filter(user__user_id = user_id).values("offer_id")
+	qs2 = OfferAppMap.objects.filter(app_id = app_id).values("offer_id")
+	qs = qs1.intersection(qs2)
+	if(qs.count()==0):
+		return json_response("Offer isn't available for given (user_id, app_id)= (%s,%s)"%(user_id, app_id))
+	offer = Offer.objects.get(pk = qs[0]['offer_id'])
+	print("Using offer: %s for given (user_id, app_id)= (%s,%s)"%(str(offer),user_id, app_id))
+	offerusermap_entry = offer.offerusermap_set.get(user__user_id = user_id)
+	user = offerusermap_entry.user
+	license_id = offer.license_id
+
+	# license update
+	query_set = License.objects.filter(offer = offer, user = user, device_id = device_id)
+	query_set_count = query_set.count()
+
+	if (query_set_count >= 2):
+		return json_response("Multiple license for same (user, device) tuple found. Something went wrong")
+	if (query_set_count  == 0):
+		# check and allocate license and add license entry
+		# mutex might be required when updating active_license_count
+		if (offer.active_license_count >= offer.max_license_count):
+			return json_response("Pool license limit reached")
+		else:
+			offer.active_license_count += 1
+			offer.save()
+			license = License.objects.create(offer = offer, user = user, device_id = device_id)
+			print("Created Pool License entry for (license_id, user_id, device_id) = (%s,%s,%s)"%(license_id, user_id, device_id))
+	elif (query_set_count  == 1):
+		license = query_set[0]
+	# add/update license app map entry
 	try:
-		offer = Offer.objects.get(license_id = license_id)
-		offerusermap_entry = offer.offerusermap_set.get(user__user_id = user_id)
-		user = offerusermap_entry.user
-		offer.offerappmap_set.get(app_id = app_id)
+		licenseappmap_entry = license.licenseappmap_set.get(app_id = app_id)
+		licenseappmap_entry.last_active_time = timezone.now()
+		licenseappmap_entry.save()
+	except (KeyError, LicenseAppMap.DoesNotExist):
+		license.licenseappmap_set.create(app_id = app_id, last_active_time = timezone.now())
+	print("Created/Updated LicenseAppMap entry due to expired heartbeat for (license_id, user_id, device_id, app_id) =  (%s,%s,%s,%s)"%(license_id, user_id, device_id, app_id))
+	# TODO: Schedule next heartbeat check and remove entry
+	return json_response("success")
 
-		# license update
-		query_set_count = License.objects.filter(offer = offer, user = user, device_id = device_id).count()
-		if (query_set_count  == 0):
-			# check and allocate license and add license entry
-			# mutex might be required when updating active_license_count
-			if (offer.active_license_count >= offer.max_license_count):
-				return HttpResponse("Pool license limit reached")
-			else:
-				offer.active_license_count += 1
-				offer.save()
-				license = License.objects.create(offer = offer, user = user, device_id = device_id)
-				print("Created Pool License entry for (license_id, user_id, device_id) =  (%s,%s,%s,%s)"%(license_id, user_id, device_id))
-		# add/update license app map entry
-		if (query_set_count == 0 or query_set_count == 1):
-			try:
-				licenseappmap_entry = license.licenseappmap_set.get(app_id = app_id)
-				licenseappmap_entry.last_active_time = timezone.now()
-				licenseappmap_entry.save()
-			except (KeyError, LicenseAppMap.DoesNotExist):
-				license.licenseappmap_set.create(app_id = app_id, last_active_time = timezone.now())
-			print("Created/Updated LicenseAppMap entry due to expired heartbeat for (license_id, user_id, device_id, app_id) =  (%s,%s,%s,%s)"%(license_id, user_id, device_id, app_id))
-			# TODO: Schedule next heartbeat check and remove entry
-		if (query_set_count >= 2):
-			return HttpResponse("Multiple license for same (user, device) tuple found. Something went wrong")
-
-	except (KeyError, Offer.DoesNotExist):
-		return HttpResponse("Offer isn't available for given license id")
-	except (KeyError, OfferUserMap.DoesNotExist):
-		return HttpResponse("Offer isn't available for given user id")
-	except (KeyError, OfferAppMap.DoesNotExist):
-		return HttpResponse("Offer isn't available for given app id")
-
-
+@csrf_exempt
 def dereg(request):
-	license_id = request.POST["license_id"]
-	user_id = request.POST["user_id"]
-	app_id = request.POST["app_id"]
-	device_id = request.POST["device_id"]
+	user_id = request.POST["userId"]
+	app_id = request.POST["nglAppId"]
+	device_id = request.POST["deviceId"]
 
 	# verify if the user, app is part of the license
-	try:
-		offer = Offer.objects.get(license_id = license_id)
-		offerusermap_entry = offer.offerusermap_set.get(user__user_id = user_id)
-		user = offerusermap_entry.user
-		offer.offerappmap_set.get(app_id = app_id)
+	qs1 = OfferUserMap.objects.filter(user__user_id = user_id).values("offer_id")
+	qs2 = OfferAppMap.objects.filter(app_id = app_id).values("offer_id")
+	qs = qs1.intersection(qs2)
+	if(qs.count()==0):
+		return json_response("Offer isn't available for given (user_id, app_id)= (%s,%s)"%(user_id, app_id), False)
+	offer = Offer.objects.get(pk = qs[0]['offer_id'])
+	print("Using offer: %s for given (user_id, app_id)= (%s,%s)"%(str(offer),user_id, app_id))
+	offerusermap_entry = offer.offerusermap_set.get(user__user_id = user_id)
+	user = offerusermap_entry.user
+	license_id = offer.license_id
 
-		# license update
-		query_set = License.objects.filter(offer = offer, user = user, device_id = device_id)
-		query_set_count = query_set.count()
-		if (query_set_count  == 0):
-			# do nothing
-			return HttpResponse("Dereg call made for non existant license entry")
+	# license update
+	query_set = License.objects.filter(offer = offer, user = user, device_id = device_id)
+	query_set_count = query_set.count()
+
+	if (query_set_count >= 2):
+		return json_response("Multiple license for same (user, device) tuple found. Something went wrong", False)
+	if (query_set_count  == 0):
+		# do nothing
+		return json_response("Dereg call made for non existant license entry", False)
+	elif (query_set_count  == 1):
+		license = query_set[0]
 		# Remove license app map entry
-		if (query_set_count == 1):
-			try:
-				licenseappmap_entry = license.licenseappmap_set.get(app_id = app_id)
+		try:
+			licenseappmap_entry = license.licenseappmap_set.get(app_id = app_id)
+			licenseappmap_entry.delete()
+			print("Removed LicenseAppMap entry due to dereg for (license_id, user_id, device_id, app_id) =  (%s,%s,%s,%s)"%(license_id, user_id, device_id, app_id))
+		except (KeyError, LicenseAppMap.DoesNotExist):
+			return json_response("Dereg call made for non existant licenceappmap entry", False)
+		if (license.licenseappmap_set.count()==0):
+			license = query_set[0]
+			license.delete()
+			if (offer.active_license_count == 0):
+				return json_response("Trying to reduce active_license_count when current active_license_count=0. Something went wrong", False)
+			offer.active_license_count -= 1
+			offer.save()
+			print("Released Pool License entry for (license_id, user_id, device_id) =  (%s,%s,%s)"%(license_id, user_id, device_id))
+	return json_response("success", False)
+
+def dereg_by_server_if_req(request, user_id, device_id, app_id):
+	# verify if the user, app is part of the license
+	qs1 = OfferUserMap.objects.filter(user__user_id = user_id).values("offer_id")
+	qs2 = OfferAppMap.objects.filter(app_id = app_id).values("offer_id")
+	qs = qs1.intersection(qs2)
+	if(qs.count()==0):
+		return json_response("Offer isn't available for given (user_id, app_id)= (%s,%s)"%(user_id, app_id), False)
+	offer = Offer.objects.get(pk = qs[0]['offer_id'])
+	print("Using offer: %s for given (user_id, app_id)= (%s,%s)"%(str(offer),user_id, app_id))
+	offerusermap_entry = offer.offerusermap_set.get(user__user_id = user_id)
+	user = offerusermap_entry.user
+	license_id = offer.license_id
+
+	# license update
+	query_set = License.objects.filter(offer = offer, user = user, device_id = device_id)
+	query_set_count = query_set.count()
+
+	if (query_set_count >= 2):
+		return json_response("Multiple license for same (user, device) tuple found. Something went wrong", False)
+	if (query_set_count  == 0):
+		# do nothing
+		return json_response("Dereg call made for non existant license entry", False)
+	elif (query_set_count  == 1):
+		license = query_set[0]
+		# Remove license app map entry
+		try:
+			licenseappmap_entry = license.licenseappmap_set.get(app_id = app_id)
+			if (licenseappmap_entry.last_active_time +  timezone.timedelta(minutes = HEARTBEAT) < timezone.now()):
 				licenseappmap_entry.delete()
-			except (KeyError, LicenseAppMap.DoesNotExist):
-				return HttpResponse("Dereg call made for non existant licenceappmap entry")
-			if (license.licenseappmap_set.count()==0):
-				try:
-					License.objects.get(offer = offer, user = user, device_id = device_id).delete();
-				except (KeyError, License.DoesNotExist):
-					return HttpResponse("Offer isn't available for given license id")
-				offer.active_license_count -= 1
-				offer.save()
-				print("Released Pool License entry for (license_id, user_id, device_id) =  (%s,%s,%s,%s)"%(license_id, user_id, device_id))
-		if (query_set_count >= 2):
-			return HttpResponse("Multiple license for same (user, device) tuple found. Something went wrong")
-
-	except (KeyError, Offer.DoesNotExist):
-		return HttpResponse("Offer isn't available for given license id")
-	except (KeyError, OfferUserMap.DoesNotExist):
-		return HttpResponse("Offer isn't available for given user id")
-	except (KeyError, OfferAppMap.DoesNotExist):
-		return HttpResponse("Offer isn't available for given app id")
-
-def dereg_by_server_if_req(request, license_id, user_id, device_id, app_id):
-	# verify if the user, app is part of the license
-	try:
-		offer = Offer.objects.get(license_id = license_id)
-		offerusermap_entry = offer.offerusermap_set.get(user__user_id = user_id)
-		user = offerusermap_entry.user
-		offer.offerappmap_set.get(app_id = app_id)
-
-		# license update
-		query_set = License.objects.filter(offer = offer, user = user, device_id = device_id)
-		query_set_count = query_set.count()
-		if (query_set_count  == 0):
-			# do nothing
-			return HttpResponse("Dereg call made for non existant license entry")
-		# Remove license app map entry
-		if (query_set_count == 1):
-			try:
-				licenseappmap_entry = license.licenseappmap_set.get(app_id = app_id)
-				if (licenseappmap_entry.last_active_time +  timezone.timedelta(minutes = HEARTBEAT+BUFFER) < timezone.now()):
-					licenseappmap_entry.delete()
-					print("Removed LicenseAppMap entry due to expired heartbeat for (license_id, user_id, device_id, app_id) =  (%s,%s,%s,%s)"%(license_id, user_id, device_id, app_id))
-			except (KeyError, LicenseAppMap.DoesNotExist):
-				return HttpResponse("Dereg call made for non existant licenceappmap entry")
-			if (license.licenseappmap_set.count()==0):
-				try:
-					License.objects.get(offer = offer, user = user, device_id = device_id).delete();
-				except (KeyError, License.DoesNotExist):
-					return HttpResponse("Offer isn't available for given license id")
-				offer.active_license_count -= 1
-				offer.save()
-				print("Released Pool License entry by server for (license_id, user_id, device_id) =  (%s,%s,%s,%s)"%(license_id, user_id, device_id))
-		if (query_set_count >= 2):
-			return HttpResponse("Multiple license for same (user, device) tuple found. Something went wrong")
-
-	except (KeyError, Offer.DoesNotExist):
-		return HttpResponse("Offer isn't available for given license id")
-	except (KeyError, OfferUserMap.DoesNotExist):
-		return HttpResponse("Offer isn't available for given user id")
-	except (KeyError, OfferAppMap.DoesNotExist):
-		return HttpResponse("Offer isn't available for given app id")
+				print("Removed LicenseAppMap entry due to expired heartbeat for (license_id, user_id, device_id, app_id) =  (%s,%s,%s,%s)"%(license_id, user_id, device_id, app_id))
+		except (KeyError, LicenseAppMap.DoesNotExist):
+			return json_response("Dereg call made for non existant licenceappmap entry", False)
+		if (license.licenseappmap_set.count()==0):
+			license = query_set[0]
+			license.delete()
+			if (offer.active_license_count == 0):
+				return json_response("Trying to reduce active_license_count when current active_license_count=0. Something went wrong", False)
+			offer.active_license_count -= 1
+			offer.save()
+			print("Released Pool License entry for (license_id, user_id, device_id) =  (%s,%s,%s)"%(license_id, user_id, device_id))
+	return json_response("Success", False)
