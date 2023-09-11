0d7129233484d5b0942cfa16957f2e9b8b8596ea
Bhavan Turaka <bturaka@adobe.com>
2023-09-12 00:49:03 +0530

    changed type of api calls from GET to POST

--- flux/views.py
+++ flux/views.py
@@ -30,9 +30,9 @@ def index(request):
 
 @csrf_exempt
 def reg(request):
-	user_id = request.GET["userId"]
-	app_id = request.GET["nglAppId"]
-	device_id = request.GET["deviceId"]
+	user_id = request.POST["userId"]
+	app_id = request.POST["nglAppId"]
+	device_id = request.POST["deviceId"]
 
 	# verify if the user, app is part of the license
 	qs1 = OfferUserMap.objects.filter(user__user_id = user_id).values("offer_id")
@@ -81,9 +81,9 @@ def reg(request):
 
 @csrf_exempt
 def heartbeat(request):
-	user_id = request.GET["userId"]
-	app_id = request.GET["nglAppId"]
-	device_id = request.GET["deviceId"]
+	user_id = request.POST["userId"]
+	app_id = request.POST["nglAppId"]
+	device_id = request.POST["deviceId"]
 
 	# verify if the user, app is part of the license
 	qs1 = OfferUserMap.objects.filter(user__user_id = user_id).values("offer_id")
@@ -131,9 +131,9 @@ def heartbeat(request):
 
 @csrf_exempt
 def dereg(request):
-	user_id = request.GET["userId"]
-	app_id = request.GET["nglAppId"]
-	device_id = request.GET["deviceId"]
+	user_id = request.POST["userId"]
+	app_id = request.POST["nglAppId"]
+	device_id = request.POST["deviceId"]
 
 	# verify if the user, app is part of the license
 	qs1 = OfferUserMap.objects.filter(user__user_id = user_id).values("offer_id")
