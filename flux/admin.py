af0c4884b6c8b75d811d980aec498a7c4f6ed0b5
Bhavan Turaka <bturaka@adobe.com>
2023-09-11 12:14:30 +0530

    Added models for DB tables

--- flux/admin.py
+++ flux/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 
-from .models import Question, Choice
+from .models import Offer, OfferAppMap, User, License, LicenseAppMap
 
-admin.site.register(Question)
-admin.site.register(Choice)
+admin.site.register(Offer)
+admin.site.register(OfferAppMap)
+admin.site.register(User)
+admin.site.register(License)
+admin.site.register(LicenseAppMap)
--- flux/models.py
+++ flux/models.py
@@ -3,18 +3,38 @@ import datetime
 from django.db import models
 from django.utils import timezone
 
-class Question(models.Model):
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField("date published")
+class Offer(models.Model):
+    license_id = models.CharField(max_length=500)
+    max_license_count = models.IntegerField()
+    active_license_count = models.IntegerField(default=0)
     def __str__(self):
-        return self.question_text
-    def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        return self.license_id
 
-
-class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
+class OfferAppMap(models.Model):
+    offer = models.ForeignKey(Offer, on_delete=models.CASCADE)
+    app_id = models.CharField(max_length=100)
+    active_license_count = models.IntegerField(default=0)
     def __str__(self):
-        return self.choice_text
+        return self.offer + " - " + self.app_id
+
+class User(models.Model):
+    user_id = models.CharField(max_length=500)
+    firstname = models.CharField(max_length=100)
+    lastname = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    def __str__(self):
+        return self.user__email
+
+class License(models.Model):
+    offer = models.ForeignKey(Offer, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    device_id = models.CharField(max_length=500)
+    def __str__(self):
+        return self.user__email + "/" + self.device_id
+
+class LicenseAppMap(models.Model):
+    license = models.ForeignKey(License, on_delete=models.CASCADE)
+    app_id = models.CharField(max_length=100)
+    last_active_time = models.DateTimeField("last active time")
+    def __str__(self):
+        return self.license + " - " + self.app_id
